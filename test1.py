211ad5b0644b24c3ec16ff3578e0e71457b66e90
daoyou.xw <daoyou.xw@alibaba-inc.com>
2019-12-03 16:32:34 +0800

    xxx

--- test1.py
+++ test1.py
@@ -3,7 +3,7 @@ from sklearn.svm import SVC
 
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
-clf = SVC()
+clf = SVC(gamma='auto')
 clf.fit(X, y) 
 print('predict [-0.8, -1]:', clf.predict([[-0.8, -1]]))
 score = clf.score([[-1, -0.5], [-1.5, -1], [1, 1.5], [2.5, 1]], [1, 1, 2, 2])
